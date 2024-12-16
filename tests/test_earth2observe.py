2a3fe736f3b634bf72681c5e6d3d174e90d49aca
Mostafa Farrag <moah.farag@gmail.com>
2024-12-16 18:23:37 +0100

    remove ecmwf tests

--- tests/test_earth2observe.py
+++ tests/test_earth2observe.py
@@ -7,7 +7,6 @@ import pytest
 
 from earth2observe.chirps import CHIRPS
 from earth2observe.earth2observe import Earth2Observe
-from earth2observe.ecmwf import ECMWF
 from earth2observe.s3 import S3
 
 
@@ -54,49 +53,6 @@ class TestChirpsBackend:
         # delete the files
         try:
             shutil.rmtree(f"{chirps_data_source_output_dir}")
-        except PermissionError:
-            print("the downloaded files could not be deleted")
-
-
-class TestECMWFBackend:
-    @pytest.fixture(scope="module")
-    def test_ecmwf_data_source_instantiate_object(
-        self,
-        ecmwf_data_source: str,
-        dates: List,
-        daily_temporal_resolution: str,
-        ecmwf_variables: List[str],
-        lat_bounds: List,
-        lon_bounds: List,
-        ecmwf_data_source_output_dir: str,
-    ):
-        e2o = Earth2Observe(
-            data_source=ecmwf_data_source,
-            start=dates[0],
-            end=dates[1],
-            variables=ecmwf_variables,
-            lat_lim=lat_bounds,
-            lon_lim=lon_bounds,
-            temporal_resolution=daily_temporal_resolution,
-            path=ecmwf_data_source_output_dir,
-        )
-        assert isinstance(e2o.DataSources, dict)
-        assert isinstance(e2o.datasource, ECMWF)
-        assert e2o.datasource.vars == ecmwf_variables
-        return e2o
-
-    def test_download_ecmwf_backend(
-        self,
-        test_ecmwf_data_source_instantiate_object: CHIRPS,
-        ecmwf_data_source_output_dir: str,
-        number_downloaded_files: int,
-    ):
-        test_ecmwf_data_source_instantiate_object.download()
-        filelist = glob.glob(os.path.join(f"{ecmwf_data_source_output_dir}", f"*.tif"))
-        assert len(filelist) == number_downloaded_files
-        # delete the files
-        try:
-            shutil.rmtree(f"{ecmwf_data_source_output_dir}")
         except PermissionError:
             print("the downloaded files could not be deleted")
 
